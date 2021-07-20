7ce3eb9575e11709d23236219f3259f8e19be807
Cilicon Ueki <79232217+cilicon128@users.noreply.github.com>
2021-07-20 17:17:29 +0900

    修正7/20

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const quiz = [
 const quizLength = quiz.length;
 let quizIndex = 0;
 let score = 0;
-
+ 
 
 const $button = document.getElementsByTagName('button')
 const buttonLength = $button.length;
